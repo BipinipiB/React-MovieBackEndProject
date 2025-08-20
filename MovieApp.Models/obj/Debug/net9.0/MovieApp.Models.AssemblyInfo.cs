fc603793672b5f81dcda1d879b0d1175cbfa0446
Bipin Byanjankar <bipin2045@yahoo.com>
2025-08-21 00:33:30 +1200

    clean architecture with modular backend and working frontend integration

--- MovieApp.Models/obj/Debug/net9.0/MovieApp.Models.AssemblyInfo.cs
+++ MovieApp.Models/obj/Debug/net9.0/MovieApp.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6889e9490ece026cee9000535bedd0d511d27bf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApp.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- MovieApp.Utility/obj/Debug/net9.0/MovieApp.Utility.AssemblyInfo.cs
+++ MovieApp.Utility/obj/Debug/net9.0/MovieApp.Utility.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApp.Utility")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6889e9490ece026cee9000535bedd0d511d27bf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApp.Utility")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApp.Utility")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
