6a824e217923dc45c85ac40bb62e116b7380a3a6
Bipin Byanjankar <bipin2045@yahoo.com>
2025-08-21 01:02:35 +1200

    backend project commits

---
+++ MovieApp.Backend/Controllers/MovieProxyController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieApp.Service.Services;
+
+namespace movie_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieProxyController : ControllerBase
+    {
+        private readonly TMDBService _tmdbService;
+
+        public MovieProxyController(TMDBService tmdbService)
+        {
+            _tmdbService = tmdbService;
+        }
+
+        // GET: api/movieproxy/popular
+        // this added "/popular" to the base route
+        [HttpGet("popular")]
+        public async Task<IActionResult> GetPopularMovies()
+        {
+            var movies = await _tmdbService.GetPopularMoviesAsync();
+            return Ok(movies);
+        }
+
+        // GET: api/movieproxy/search?query=inception
+        [HttpGet("search")]
+        public async Task<IActionResult> SearchMovies([FromQuery] string query)
+        {
+            var results = await _tmdbService.SearchMoviesAsync(query);
+            return Ok(results);
+        }
+
+    }
+}
---
+++ MovieApp.Backend/Program.cs
@@ -0,0 +1,100 @@
+using Microsoft.EntityFrameworkCore;
+using MovieApp.DataAccess.Data;
+using MovieApp.DataAccess.Repository;
+using MovieApp.DataAccess.Repository.IRepository;
+using MovieApp.Service.Services;
+using MovieApp.Services.Interfaces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+////register the HTTPclient..this tells us call TMDB
+builder.Services.AddHttpClient<TMDBService>();
+
+//register the TMDBRepository with the ITMDBRepository interface
+//AddHttpsClient becasuee this interface is used to call TMDB API
+builder.Services.AddHttpClient<TMDBRepository>();
+
+
+//register the TMDBService
+builder.Services.AddScoped<TMDBService>();
+
+//register the IUserRepository with the UserRepository implementation
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+//register the UserService
+builder.Services.AddScoped<UserService>();
+
+//register swagger services
+builder.Services.AddEndpointsApiExplorer();
+//swagger is helpful for documentation and testing
+builder.Services.AddSwaggerGen();
+
+//Enable CORS
+//CORS allows frontend origins
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        builder => builder
+            .WithOrigins("http://localhost:5173") // React dev server "http://localhost:5174",
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+
+//register ApplicationDbContext
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//register IMovieRepository with MovieRepository implementation
+builder.Services.AddScoped<IMovieRepository, MovieRepository>();
+
+//register ITMDBRepository with TMDBRepository implementation
+builder.Services.AddScoped<ITMDBRepository, TMDBRepository>();
+
+//register IMovieService
+builder.Services.AddScoped<IMovieService, MovieService>();
+
+//register MovieSyncService.
+//registered MovieSyncService even though it is not Interface because
+//it is used to run a background job with Hangfire
+builder.Services.AddScoped<MovieSyncService>();
+
+//register hangfire
+//builder.Services.AddHangfire(config =>
+//    config.UseSqlServerStorage(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//builder.Services.AddHangfireServer();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//apply CORS
+app.UseCors("AllowReactApp");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
